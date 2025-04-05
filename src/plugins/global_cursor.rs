06335c1736abf140f38806fc0c3f87a7b1b9f51a
Zoe <tangenzoe@gmail.com>
2025-04-05 21:44:42 +0100

    debug

--- src/plugins/global_cursor.rs
+++ src/plugins/global_cursor.rs
@@ -6,6 +6,7 @@ pub struct GlobalCursorPlugin;
 impl Plugin for GlobalCursorPlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<GlobalCursorPosition>()
+            // .add_systems(Update, print_global_cursor_position)
             .add_systems(Update, track_global_cursor_position);
     }
 }
@@ -18,4 +19,8 @@ fn track_global_cursor_position(mut global_pos: ResMut<GlobalCursorPosition>) {
     if let Mouse::Position { x, y } = mouse {
         global_pos.0 = Some(Vec2::new(x as f32, y as f32));
     }
+}
+
+fn print_global_cursor_position(global_pos: Res<GlobalCursorPosition>) {
+    info!("Position: {:?}", global_pos.0);
 }
