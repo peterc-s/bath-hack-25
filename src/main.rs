bebe5fb53295f18c449e160acb97e5f0f115c66b
peterc-s <petercs@purelymail.com>
2025-04-05 13:42:39 +0100

    restart with bevy

--- src/main.rs
+++ src/main.rs
@@ -1,182 +1,35 @@
-use std::sync::Arc;
+use bevy::prelude::*;
 
-use winit::{
-    application::ApplicationHandler,
-    event::WindowEvent,
-    event_loop::{ActiveEventLoop, ControlFlow, EventLoop},
-    window::{Window, WindowId},
-};
-
-#[derive(Debug)]
-struct State {
-    window: Arc<Window>,
-    device: wgpu::Device,
-    queue: wgpu::Queue,
-    size: winit::dpi::PhysicalSize<u32>,
-    surface: wgpu::Surface<'static>,
-    surface_format: wgpu::TextureFormat,
+fn main() {
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        transparent: true,
+                        decorations: false,
+                        resizable: false,
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .set(ImagePlugin::default_nearest())
+        )
+        .insert_resource(ClearColor(Color::NONE))
+        .add_systems(Startup, setup)
+        .run();
 }
 
-use anyhow::{Result, anyhow};
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2d::default());
 
-impl State {
-    async fn new(window: Arc<Window>) -> Result<State> {
-        let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor::default());
+    let mut bonnie_sprite = Sprite::from_image(
+        asset_server.load("bonnietest.png")
+    );
 
-        let adapter = instance
-            .request_adapter(&wgpu::RequestAdapterOptions::default())
-            .await
-            .ok_or(anyhow!("Couldn't get adapter."))?;
+    bonnie_sprite.custom_size = Some(Vec2::new(100.0, 100.0));
 
-        let (device, queue) = adapter
-            .request_device(&wgpu::DeviceDescriptor::default(), None)
-            .await?;
-
-        let size = window.inner_size();
-
-        let surface = instance.create_surface(window.clone()).unwrap();
-        let cap = surface.get_capabilities(&adapter);
-        let surface_format = cap.formats[0];
-
-        let state = State {
-            window,
-            device,
-            queue,
-            size,
-            surface,
-            surface_format,
-        };
-
-        // configure surface for the first time
-        state.configure_surface();
-
-        Ok(state)
-    }
-
-    fn get_window(&self) -> &Window {
-        &self.window
-    }
-
-    fn configure_surface(&self) {
-        let surface_config = wgpu::SurfaceConfiguration {
-            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-            format: self.surface_format,
-            view_formats: vec![self.surface_format.add_srgb_suffix()],
-            alpha_mode: wgpu::CompositeAlphaMode::Auto,
-            width: self.size.width,
-            height: self.size.height,
-            desired_maximum_frame_latency: 2,
-            present_mode: wgpu::PresentMode::AutoVsync,
-        };
-        self.surface.configure(&self.device, &surface_config);
-    }
-
-    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
-        self.size = new_size;
-
-        // reconfigure the surface
-        self.configure_surface();
-    }
-
-    fn render(&mut self) {
-        // create texture view
-        let surface_texture = self
-            .surface
-            .get_current_texture()
-            .expect("failed to acquire next swapchain texture");
-        let texture_view = surface_texture
-            .texture
-            .create_view(&wgpu::TextureViewDescriptor {
-                // without add_srgb_suffix() the image we will be working with
-                // might not be "gamma correct".
-                format: Some(self.surface_format.add_srgb_suffix()),
-                ..Default::default()
-            });
-
-        // renders a GREEN screen
-        let mut encoder = self.device.create_command_encoder(&Default::default());
-        // create the renderpass which will clear the screen.
-        let renderpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-            label: None,
-            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                view: &texture_view,
-                resolve_target: None,
-                ops: wgpu::Operations {
-                    load: wgpu::LoadOp::Clear(wgpu::Color::GREEN),
-                    store: wgpu::StoreOp::Store,
-                },
-            })],
-            depth_stencil_attachment: None,
-            timestamp_writes: None,
-            occlusion_query_set: None,
-        });
-
-        // end the renderpass.
-        drop(renderpass);
-
-        // submit the command in the queue to execute
-        self.queue.submit([encoder.finish()]);
-        self.window.pre_present_notify();
-        surface_texture.present();
-    }
-}
-
-#[derive(Default, Debug)]
-struct App {
-    state: Option<State>,
-}
-
-impl ApplicationHandler for App {
-    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        // create window object
-        let window = Arc::new(
-            event_loop
-                .create_window(
-                    Window::default_attributes()
-                        .with_transparent(true)
-                        .with_resizable(false)
-                        .with_title("Bonnie Buddy")
-                        .with_decorations(false)
-                )
-                .expect("Couldn't create window."),
-        );
-
-        let state = pollster::block_on(State::new(window.clone()));
-        self.state = Some(state.expect("Didn't get state."));
-
-        window.request_redraw();
-    }
-
-    fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
-        let state = self.state.as_mut().unwrap();
-        match event {
-            WindowEvent::CloseRequested => {
-                println!("The close button was pressed; stopping");
-                event_loop.exit();
-            }
-            WindowEvent::RedrawRequested => {
-                state.render();
-                // emits a new redraw requested event.
-                state.get_window().request_redraw();
-            }
-            WindowEvent::Resized(size) => {
-                // reconfigures the size of the surface.
-                state.resize(size);
-            }
-            _ => (),
-        }
-    }
-}
-
-fn main() -> Result<()> {
-    env_logger::init();
-
-    let event_loop = EventLoop::new()?;
-
-    event_loop.set_control_flow(ControlFlow::Poll);
-
-    let mut app = App::default();
-    event_loop.run_app(&mut app)?;
-
-    Ok(())
+    commands.spawn(
+        bonnie_sprite
+    );
 }
